6198cafd897de04bc38fc4eddc36a49eff5e77f1
agroce <agroce@gmail.com>
2024-06-13 07:33:10 -0700

    script now standalone

--- subset_corpus.py
+++ subset_corpus.py
@@ -1,9 +1,74 @@
 import glob
 import os
 import shutil
+import signal
+import subprocess
 import sys
+import time
+from contextlib import contextmanager
 
-from muttfuzz.fuzzutil import silent_run_with_timeout
+
+class TimeoutException(Exception):
+    """"Exception thrown when timeouts occur"""
+
+
+@contextmanager
+def time_limit(seconds):
+    """Method to define a time limit before throwing exception"""
+
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+def silent_run_with_timeout(cmd, timeout, verbose):
+    # Allow functions instead of commands, for use as a library from a script
+    if verbose:
+        print("*" * 30)
+    if callable(cmd):
+        try:
+            if verbose:
+                print("CALLING FUNCTION", cmd)
+            with time_limit(timeout):
+                return cmd()
+        except TimeoutException:
+            print("ABORTED WITH TIMEOUT")
+            return 1 # non-zero return code may be interpreted as failure/crash/timeout
+    dnull = open(os.devnull, 'w')
+    if verbose:
+        print("EXECUTING", cmd)
+    start_P = time.time()
+    try:
+        with open("cmd_errors.txt", 'w') as cmd_errors:
+            P = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid,
+                                 stdout=dnull, stderr=cmd_errors)
+            while (P.poll() is None) and ((time.time() - start_P) < timeout):
+                time.sleep(min(0.5, timeout / 10.0)) # Allow for small timeouts
+            if P.poll() is None:
+                os.killpg(os.getpgid(P.pid), signal.SIGTERM)
+        with open("cmd_errors.txt", 'r') as cmd_errors:
+            try:
+                cmd_errors_out = cmd_errors.read()
+            except:
+                cmd_errors_out = "ERROR READING OUTPUT"
+        if verbose and len(cmd_errors_out) > 0:
+            print("OUTPUT (TRUNCATED TO LAST 20 LINES):")
+            print("\n".join(cmd_errors_out.split("\n")[-20:]))
+    finally:
+        if P.poll() is None:
+            print("KILLING SUBPROCESS DUE TO TIMEOUT")
+            os.killpg(os.getpgid(P.pid), signal.SIGTERM)
+    if verbose:
+        print("COMPLETE IN", round(time.time() - start_P, 2), "SECONDS")
+        print("*" * 30)
+
+    return P.returncode
+
 
 fuzz_cmd = sys.argv[1] # Assume appending filename to the end of this will fuzz the right input
 timeout = int(sys.argv[2])
