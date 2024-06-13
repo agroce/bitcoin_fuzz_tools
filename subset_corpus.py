2fd90cdb861df2ee35bcc560cd087ca36802ce1b
agroce <agroce@gmail.com>
2024-06-13 08:31:56 -0700

    try binary split

--- subset_corpus.py
+++ subset_corpus.py
@@ -5,40 +5,13 @@ import signal
 import subprocess
 import sys
 import time
-from contextlib import contextmanager
 
 
 class TimeoutException(Exception):
     """"Exception thrown when timeouts occur"""
 
 
-@contextmanager
-def time_limit(seconds):
-    """Method to define a time limit before throwing exception"""
-
-    def signal_handler(signum, frame):
-        raise TimeoutException("Timed out!")
-
-    signal.signal(signal.SIGALRM, signal_handler)
-    signal.alarm(seconds)
-    try:
-        yield
-    finally:
-        signal.alarm(0)
-
 def silent_run_with_timeout(cmd, timeout, verbose):
-    # Allow functions instead of commands, for use as a library from a script
-    if verbose:
-        print("*" * 30)
-    if callable(cmd):
-        try:
-            if verbose:
-                print("CALLING FUNCTION", cmd)
-            with time_limit(timeout):
-                return cmd()
-        except TimeoutException:
-            print("ABORTED WITH TIMEOUT")
-            return 1 # non-zero return code may be interpreted as failure/crash/timeout
     dnull = open(os.devnull, 'w')
     if verbose:
         print("EXECUTING", cmd)
@@ -70,6 +43,32 @@ def silent_run_with_timeout(cmd, timeout, verbose):
     return P.returncode
 
 
+def files_ok(fuzz_cmd, timeout, files, new_corpus_dir):
+    start = time.time()
+    # Returns True only if the tests pass for this whole set; restores directory to clean state
+    for f in files:
+        shutil.copyfile(f, os.path.join(new_corpus_dir, os.path.basename(f)))
+    r = silent_run_with_timeout(fuzz_cmd + " " + new_corpus_dir + "/*", timeout)
+    for f in files:
+        os.remove(os.path.join(new_corpus_dir, os.path.basename(f)))
+    finish = time.time()
+    print("ANALYZED", len(files), "IN", round(finish - start, 2), "SECONDS")
+    return (r == 0)
+
+
+def make_splits(files, N):
+    splits = []
+    start = 0
+    size = int(len(files) / N)
+    for i in range(N):
+        if i < N - 1:
+            splits.append(files[start:start + size])
+        else:
+            splits.append(files[start:])
+        start += size
+    return splits
+
+
 fuzz_cmd = sys.argv[1] # Assume appending filename to the end of this will fuzz the right input
 timeout = int(sys.argv[2])
 corpus_dir = sys.argv[3]
@@ -84,20 +83,27 @@ sys.stdout.flush()
 if not os.path.exists(new_corpus_dir):
     os.mkdir(new_corpus_dir)
 
+ok_files = []
+bad_files = []
+unknown = files
+
+SPLIT_SIZE = 4 # Initially let's just quarter the corpus
+
 copied = 0
 skipped = 0
-for f in files:
-    start = time.time()
-    r = silent_run_with_timeout(fuzz_cmd + " " + f, timeout, False)
-    finish = time.time()
-    print("ANALYZED", f, "IN", round(finish - start, 2), "SECONDS")
-    if r == 0:
-        shutil.copyfile(f, os.path.join(new_corpus_dir, os.path.basename(f)))
-        copied += 1
-    else:
-        print(f, "DETECTS THE MUTANT!")
-        skipped += 1
-sys.stdout.flush()
+while len(unknown) > 0:
+    unknown = []
+    splits = make_splits(unknown, SPLIT_SIZE)
+    for s in splits:
+        ok = files_ok(fuzz_cmd, timeout, s, new_corpus_dir)
+        if ok:
+            copied += len(s)
+        else:
+            if len(s) > 1:
+                unknown.extend(s)
+            else:
+                skipped += 1
+    sys.stdout.flush()
 
 print("THERE WERE", skipped, "DETECTING INPUTS")
 print("FINISHED, NEW CORPUS HAS", copied, "FILES")
