0fe8a4f9eea98fde47c48e88df32e8ecfe1924bc
agroce <agroce@gmail.com>
2024-06-17 13:33:02 -0700

    run

---
+++ pruneandrun.py
@@ -0,0 +1,46 @@
+import glob
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+mutant_dir = sys.argv[1]
+
+modes = ["", "-use_value_profile=1"]
+
+data = {}
+for mode in modes:
+    data[mode] = []
+
+mutants = glob.glob(mutant_dir + "/survived_*")
+mutants.extend(glob.glob(mutant_dir + "killed_*"))
+
+for m in mutants:
+    dir = os.path.basename(m) + "_pruned"
+    if not os.path.exists(dir):
+        print("PRUNING FOR", dir)
+        os.mkdir(dir)
+        subprocess.call(["python3 ../bitcoin_fuzz_tools/libfuzzer_prune.py " + m + " 700 ../qa-assets/fuzz_seed_corpus/process_messages " + dir + " 1 1000"],
+                            shell=True)
+
+print("DONE PRUNING!")
+print()
+
+RUNS = 10
+
+for r in range(RUNS):
+    for m in mutants:
+        for mode in modes:
+            print("RUNNING", m, mode)
+            shutil.rmtree("this_corpus")
+            shutil.copytree(os.path.basename(m) + "_pruned", "this_corpus")
+            start = time.time()        
+            subprocess.call([m + " " + mode + " this_corpus"], shell=True)
+            finish = time.time()
+            print("FINISHED IN", round(finish-start, 2))
+            data[mode].appedn(finish-start)
+            print(mode, data[mode])
+        
+for mode in modes:
+    print(mode, data[mode])
